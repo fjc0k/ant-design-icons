d2ed2ed55a17e4050d81fa6b3f4b71beb788dcde
fjc0k <fjc0kb@gmail.com>
2018-08-20 21:38:13 +0800

    feat: add @ant-design/icons

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   version: {
     desktop: '3.7.3',
-    mobile: '2.2.2'
+    mobile: '2.2.2',
+    standalone: 'dev'
   }
 }
---
+++ scripts/extractStandaloneIcons.js
@@ -0,0 +1,56 @@
+/* eslint new-cap: 0 no-eval: 0 */
+const path = require('path')
+const fs = require('fs-extra')
+const axios = require('axios')
+const extractZip = require('extract-zip')
+const globby = require('globby')
+const { version } = require('./config')
+
+class ExtractStandaloneIcons {
+  constructor(version) {
+    this.version = version
+    this.zipUrl = `https://github.com/ant-design/ant-design-icons/archive/${version}.zip`
+    this.targetPath = path.resolve(__dirname, '../src/standalone')
+    this.targetSVGPath = path.resolve(this.targetPath, 'svg')
+  }
+
+  async extract() {
+    const tmpPath = path.resolve(__dirname, '__tmp3')
+
+    fs.emptyDirSync(this.targetPath)
+
+    const zip = await this.fetch(this.zipUrl)
+
+    const zipFile = path.join(tmpPath, 'repo.zip')
+    const repoDir = path.join(tmpPath, 'repo')
+
+    fs.outputFileSync(zipFile, zip)
+
+    extractZip(zipFile, { dir: repoDir }, err => {
+      if (err) throw err
+
+      globby([repoDir + '/*/src/svg/*.svg'])
+        .then(svgFiles => {
+          svgFiles.forEach(svgFile => {
+            const { name, ext, dir } = path.parse(svgFile)
+            const newName = name.toLowerCase().replace(/\s+/g, '-')
+            fs.renameSync(svgFile, path.join(dir, newName + ext))
+          })
+          fs.moveSync(path.parse(svgFiles[0]).dir, this.targetSVGPath)
+          fs.removeSync(tmpPath)
+        })
+    })
+  }
+
+  async fetch(url) {
+    const { data } = await axios.get(url, { responseType: 'arraybuffer' })
+    return data
+  }
+}
+
+new ExtractStandaloneIcons(version.standalone)
+  .extract()
+  .then(
+    () => console.log('success'),
+    console.error
+  )
